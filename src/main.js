80fc7d41f7a5fe8d3241d40bbd269c5874e88647
pmbfsa <paulomarcioba@gmail.com>
2026-06-02 22:57:19 -0300

    Add vite plugin webfontdl Add font Barlow Add main java script file

--- src/main.js
+++ src/main.js
@@ -1,1 +1,68 @@
 import './styles/style.css';
+
+// Global Variables
+let mediaStream = null;
+
+// DOM Elements
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+async function startPictureInPicture() {
+  try {
+    await videoElement.requestPictureInPicture();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function clearMediaStream() {
+  videoElement.srcObject = null;
+
+  mediaStream?.getTracks().forEach((track) => track.stop());
+  mediaStream = null;
+
+  if (document.pictureInPictureElement) {
+    document.exitPictureInPicture();
+  }
+}
+
+async function selectMediaStream() {
+  clearMediaStream();
+
+  const controller = new CaptureController();
+  const displayMediaOptions = {
+    video: {
+      displaySurface: 'browser',
+      frameRate: { ideal: 60 },
+    },
+    audio: {
+      noiseSuppression: true,
+    },
+    preferCurrentTab: false,
+    selfBrowserSurface: 'exclude',
+    systemAudio: 'include',
+    surfaceSwitching: 'include',
+    monitorTypeSurfaces: 'include',
+    controller,
+  };
+
+  // Don't change focus to captured screen
+  controller.setFocusBehavior('no-focus-change');
+
+  try {
+    // Prompt capture screen
+    mediaStream =
+      await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+
+    mediaStream.getVideoTracks()[0].addEventListener('ended', clearMediaStream);
+
+    videoElement.srcObject = mediaStream;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Event listener funtions
+videoElement.addEventListener('loadedmetadata', startPictureInPicture);
+videoElement.addEventListener('leavepictureinpicture', clearMediaStream);
+button.addEventListener('click', selectMediaStream);
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
 import { defineConfig } from 'vite';
+import webfontDownload from 'vite-plugin-webfont-dl';
 
 export default defineConfig({
   base: './',
+  plugins: [
+    webfontDownload([
+      'https://fonts.googleapis.com/css2?family=Barlow&display=swap',
+    ]),
+  ],
 });
